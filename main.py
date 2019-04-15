e330eb44d4279dfa7af378ce12df393fc6758744
RICARDO RIQUELME <ririquel@alumnos.ubiobio.cl>
2019-04-14 22:02:57 -0400

    App final detección agresividad

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 import wx
+# from tkinter import *
+# from tkinter.ttk import *
+# from tkinter import filedialog
+from sklearn.model_selection import cross_val_score
 from sklearn.naive_bayes import BernoulliNB
 from sklearn import tree
 from sklearn.ensemble import RandomForestClassifier
@@ -21,6 +25,9 @@ class MiAplicacion(wx.Frame):
     txt2 = ''
     vectorizer = ''
     log = ''
+    Nuevas_entradas = ''
+    metrica1=""
+    metrica2=""
     # FUNCIONES
     # ----------------------------------------------------------------------------
 
@@ -54,7 +61,7 @@ class MiAplicacion(wx.Frame):
         return vector_train, vector_test, vectorizer
 
     def preparar(self):
-        self.addToLog("Prepararando datos...")
+        self.addToLog("----------COMENTARIOS EVALUADOS----------")
         # 1. CARGAR DATOS encuesta con cabecera texto, clase
         data = pd.read_csv(
             'sinSWsinPOS.csv',
@@ -67,6 +74,7 @@ class MiAplicacion(wx.Frame):
 
         # 3. entrenamiento
         xtrain, xtest, ytrain, ytest = tts(corpus, data['B'], test_size=0.30)
+        # xtrain, xtest, ytrain, ytest = tts(corpus, data['B'], train_size=1469)
 
         # 4. TOKENIZACION + VECTORIZACION
         global vectorizer
@@ -75,68 +83,105 @@ class MiAplicacion(wx.Frame):
 
         # 5. MODELO SVM
         global modelo
-        modelo=BernoulliNB()
+        # modelo=BernoulliNB()
         # modelo=tree.DecisionTreeClassifier(criterion='entropy',max_depth=29)
         # modelo=RandomForestClassifier(n_estimators=10)
-        # modelo = svm.SVC(kernel='linear')
-        # modelo.fit(X=xtrain, y=ytrain)
-        modelo.fit(X=xtrain.toarray(), y=ytrain)
+        modelo = svm.SVC(kernel='linear')
+        modelo.fit(X=xtrain, y=ytrain)
+        # modelo.fit(X=xtrain.toarray(), y=ytrain)
         # print(modelo.tree_.max_depth)
 
         # 6. PREDICT + METRICAS
         prediccion = modelo.predict(xtest)
 
-        self.addToLog(pd.crosstab(ytest, prediccion, rownames=[
-            'REAL'], colnames=['PREDICCION']).to_string())
-        self.addToLog("")
-        self.addToLog(classification_report(ytest, prediccion))
+        # precisionSVM = cross_val_score(estimator=modelo,X=xtrain, y=ytrain,cv=10)
+        # print('Precision SVM LINEAL: {}'.format(precisionSVM))
+        # print('Precision SVM LINEAL promedio: {0: .3f} +/- {1: .3f}'.format(np.mean(precisionSVM),np.std(precisionSVM)))
+        # self.addToLog("")
+        # self.addToLog('Precision SVM LINEAL: {}'.format(precisionSVM))
+        # self.addToLog('Precision SVM LINEAL promedio: {0: .3f} +/- {1: .3f}'.format(np.mean(precisionSVM),np.std(precisionSVM)))
+        # self.addToLog("")
+        # self.addToLog(pd.crosstab(ytest, prediccion, rownames=[
+        #     'REAL'], colnames=['PREDICCION']).to_string())
+        # self.addToLog("")
+        # self.addToLog(classification_report(ytest, prediccion))
+        global metrica1
+        global metrica2
+        metrica1 = pd.crosstab(ytest, prediccion, rownames=['REAL'], colnames=['PREDICCION']).to_string()
+        metrica2 = classification_report(ytest, prediccion)
 
     def procesar(self, texto):
         Xnew = [texto]
         vector_new = vectorizer.transform(Xnew)
         ynew = modelo.predict(vector_new)
         for i in range(len(Xnew)):
-            self.addToLog("Comentario=%s, Predicción=%s" % (Xnew[i], ynew[i]))
+            self.addToLog("%s, %s" % (Xnew[i], ynew[i]))
+
+
 
     def __init__(self, parent, title):
         print("Creando interfaz grafica...")
-        wx.Frame.__init__(self, parent=parent, title=title, size=(900, 500))
+        wx.Frame.__init__(self, parent=parent, title=title, size=(800, 600))
 
         # Paneles
         p1 = wx.Panel(self)
         p2 = wx.Panel(self)
+        p3 = wx.Panel(self)
+
 
         # Sizers
         mainsz = wx.BoxSizer(wx.VERTICAL)
         p1sz = wx.BoxSizer(wx.HORIZONTAL)
         p2sz = wx.BoxSizer(wx.HORIZONTAL)
+        p3sz = wx.BoxSizer(wx.HORIZONTAL)
+
 
         # Controles
         st1 = wx.StaticText(p1, -1, "Ingresar Comentario")
         global txt1, txt2
-        txt1 = wx.TextCtrl(p1, -1)
+        txt1 = wx.TextCtrl(p1, -1, style=wx.TE_MULTILINE) 
         st2 = wx.StaticText(p2, -1, "Métricas y Predicción")
         txt2 = wx.TextCtrl(p2, -1, style=wx.TE_READONLY | wx.TE_MULTILINE)
-        bt = wx.Button(self, -1, " Evaluar ")
+        bt = wx.Button(p3, wx.ID_ANY, " Evaluar ", pos=(100,10))
+        bt1 = wx.Button(p3, wx.ID_ANY, " Cargar Archivo ")
+        bt2 = wx.Button(p3, wx.ID_ANY, " Guardar Evaluación ")
+        bt3 = wx.Button(p3, wx.ID_ANY, " Guardar Métrica y Evaluación ")
 
         # Agregar elementos a los sizers
         p1sz.Add(st1, 1, wx.EXPAND | wx.ALL, 20)
         p1sz.Add(txt1, 4, wx.EXPAND | wx.ALL, 20)
         p2sz.Add(st2, 1, wx.EXPAND | wx.ALL, 20)
         p2sz.Add(txt2, 4, wx.EXPAND | wx.ALL, 20)
-        mainsz.Add(p1, 1, wx.EXPAND)
-        mainsz.Add(p2, 3, wx.EXPAND)
-        mainsz.Add(bt, 0, wx.ALIGN_CENTRE | wx.ALL, 20)
+        p3sz.Add(bt, -1, wx.ALIGN_BOTTOM | wx.ALL, 20)
+        p3sz.Add(bt1, -1, wx.ALIGN_BOTTOM | wx.ALL, 20)
+        p3sz.Add(bt2, -1, wx.ALIGN_BOTTOM | wx.ALL, 20)
+        p3sz.Add(bt3, -1, wx.ALIGN_BOTTOM | wx.ALL, 20)
+        mainsz.Add(p1, 2, wx.EXPAND)
+        mainsz.Add(p2, 4, wx.EXPAND)
+        mainsz.Add(p3, 2, wx.EXPAND)
+
         bt.Bind(wx.EVT_BUTTON, self.OnClicked)
+        bt1.Bind(wx.EVT_BUTTON, self.LecturaTxt)
+        bt2.Bind(wx.EVT_BUTTON, self.EscrituraTxt)
+        bt3.Bind(wx.EVT_BUTTON, self.EscrituraTxt2)
+       
 
         # Conf. sizers
         p1.SetSizer(p1sz)
         p2.SetSizer(p2sz)
+        p3.SetSizer(p3sz)
+        
+
+        imagen = wx.StaticBitmap(p3, -1, wx.Bitmap('LogoSomos.jpeg', wx.BITMAP_TYPE_ANY), pos = wx.Point (400,0), size = (347,80))
+        imagen = wx.StaticBitmap(p3, -1, wx.Bitmap('LogoUbb.jpeg', wx.BITMAP_TYPE_ANY), pos = wx.Point (30,0), size = (347,80))
+
+
+
         self.SetSizer(mainsz)
 
         self.SetBackgroundColour(p1.GetBackgroundColour())
 
-        self.Centre(True)
+        self.Centre()
         self.Show()
         self.preparar()
 
@@ -145,6 +190,45 @@ class MiAplicacion(wx.Frame):
         txt1.SetValue("")
 
 
+    def LecturaTxt(self, event):
+        filename = wx.FileDialog(frame, "Open", "", "", "Archivo de texto|*.txt", wx.FD_OPEN | wx.FD_FILE_MUST_EXIST)
+        filename.ShowModal()
+        archivo = open(filename.GetPath(), "r")
+        lectura=archivo.read()
+        print(lectura)
+        Nuevas_entradas=lectura.split(sep='\n')
+        i=0
+        while i< len(Nuevas_entradas):
+           self.procesar(Nuevas_entradas[i])
+           i=i+1
+
+    def EscrituraTxt(self, event):
+        filename = wx.FileDialog(self, "Guardar Archivo", wildcard="Archivo de Texto (.txt)|.txt",
+                       style=wx.FD_SAVE | wx.FD_OVERWRITE_PROMPT)
+      
+        filename.ShowModal()
+        if filename:
+            f = open(filename.GetPath(), 'a')
+            SALIDA = txt2.GetValue()
+            SALIDAAUX = ''.join(SALIDA)
+            f.write(SALIDAAUX)
+            f.close()
+    
+    def EscrituraTxt2(self, event):
+        filename = wx.FileDialog(self, "Guardar Archivo", wildcard="Archivo de Texto (.txt)|.txt",
+                       style=wx.FD_SAVE | wx.FD_OVERWRITE_PROMPT)
+      
+        filename.ShowModal()
+        if filename:
+            f = open(filename.GetPath(), 'a')
+            SALIDA = metrica1+'\n\n'+metrica2+'\n\n'+txt2.GetValue()
+            
+            SALIDAAUX = ''.join(SALIDA)
+            f.write(SALIDAAUX)
+            f.close()
+
+
+# Inicializa pantalla principal
 if __name__ == '__main__':
     app = wx.App()
     frame = MiAplicacion(None, u"PREDICTOR DE AGRESIVIDAD")
